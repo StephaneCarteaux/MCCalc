28965690a89d16485e6f2e989a8af14dbf3ed603
Stéphane Carteaux <stephanecarteaux@gmail.com>
2019-06-08 19:06:54 +0200

    8/06/19

--- MCCalc/ViewController.swift
+++ MCCalc/ViewController.swift
@@ -9,7 +9,54 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    //MARK: Properties
+    @IBOutlet weak var numeroPreguntasTextField: NSTextField!
+    @IBOutlet weak var numeroOpcionesTextField: NSTextField!
+    @IBOutlet weak var correctasTextField: NSTextField!
+    @IBOutlet weak var incorrectasTextField: NSTextField!
+    @IBOutlet weak var notaLabel: NSTextField!
+    
+    enum CalcularError: Error {
+        case generalError
+        case opcionesError
+    }
+    
+    func calcular () throws {
+        guard let preguntas:Float = Float(numeroPreguntasTextField.stringValue) else {
+            throw CalcularError.generalError
+        }
+        guard let opciones:Float = Float(numeroOpcionesTextField.stringValue) else {
+            throw CalcularError.generalError
+        }
+        guard opciones >= 2 else {
+            throw CalcularError.opcionesError
+        }
+        guard let correctas:Float = Float(correctasTextField.stringValue) else {
+            throw CalcularError.generalError
+        }
+        guard let incorrectas:Float = Float(incorrectasTextField.stringValue) else {
+            throw CalcularError.generalError
+        }
+        guard correctas + incorrectas <= preguntas else {
+            throw CalcularError.generalError
+        }
+        let nota:Float = (Float((correctas - (incorrectas / (opciones - 1))) / preguntas * 10))
+        notaLabel.stringValue = String(format: "%.2f", round(nota/0.25)*0.25)
+        
+    }
+    
+    //MARK: Actions
+    @IBAction func calcularButton(_ sender: NSButton) {
+        do {
+            try calcular ()
+        } catch CalcularError.opcionesError {
+            notaLabel.stringValue = ("Error")
+        } catch {
+            notaLabel.stringValue = ("Error")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
